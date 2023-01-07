0861bb7fbd10032cb8ee24e8e6d7e3a9a39daa58
Samuel <guilherme.engr@gmail.com>
2023-01-07 04:02:02 -0300

    adjust font in project

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -9,18 +9,14 @@ export const GlobalStyle = createGlobalStyle`
   html,body {
     margin: 0;
     padding: 0;
-    font-family: 'Inter', Helvetica, Sans-Serif;
+    font-family: 'Inter', Sans-Serif;
   }
 
-  @font-face {
-  font-family: "Inter"; 
-  src: local("Inter-Regular"), url("../assets/fonts/Inter-Regular.ttf") format("truetype");
-  }
 `;
 
 export const TextDefault = styled.span`
   color: #111827;
-  font-family: 'Inter', sans-serif;
+  font-family: Inter, sans-serif;
   font-size: 1rem;
   font-weight: 400;
   text-decoration: none;
